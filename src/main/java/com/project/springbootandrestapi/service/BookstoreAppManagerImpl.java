ca73d0c003a13bff6e114d83ffddedd44eadebbe
HumeyraUnlu <75032647+HumeyraUnlu@users.noreply.github.com>
2021-07-26 15:46:58 +0300

    Update BookstoreAppManagerImpl.java

--- src/main/java/com/project/springbootandrestapi/service/BookstoreAppManagerImpl.java
+++ src/main/java/com/project/springbootandrestapi/service/BookstoreAppManagerImpl.java
@@ -2,7 +2,6 @@ package com.project.springbootandrestapi.service;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,10 +9,10 @@ import com.project.springbootandrestapi.dao.BookstoreAppDao;
 import com.project.springbootandrestapi.entities.BookstoreApp;
 
 @Service
-public class BookstoreAppManagerImpl implements BookstoreAppService{
+public class BookstoreAppManagerImpl implements BookstoreAppService {
 
 	private BookstoreAppDao bookstoreAppDao;
-	
+
 	@Autowired
 	public BookstoreAppManagerImpl(BookstoreAppDao bookstoreAppDao) {
 		this.bookstoreAppDao = bookstoreAppDao;
@@ -26,25 +25,30 @@ public class BookstoreAppManagerImpl implements BookstoreAppService{
 	}
 
 	@Override
+	public BookstoreApp getById(int id) {
+		return this.bookstoreAppDao.getById(id);
+
+	}
+
+	@Override
 	@Transactional
 	public void add(BookstoreApp book) {
 		this.bookstoreAppDao.add(book);
 	}
-	
 
 	@Override
 	@Transactional
 	public void update(BookstoreApp book) {
-		this.bookstoreAppDao.update(book);		
+		this.bookstoreAppDao.update(book);
 	}
 
 	@Override
 	@Transactional
 	public void delete(BookstoreApp book) {
-		this.bookstoreAppDao.delete(book);
+			this.bookstoreAppDao.delete(book);
 		
-	}
 
+	}
 
 	@Override
 	public List<BookstoreApp> getCategory(String category) {
@@ -67,4 +71,10 @@ public class BookstoreAppManagerImpl implements BookstoreAppService{
 	public List<String> getBookStores() {
 		return this.bookstoreAppDao.getBookStores();
 	}
+
+	@Override
+	public List<BookstoreApp> getBookPrices(String name) {
+		return this.bookstoreAppDao.getBookPrices(name);
+	}
+
 }
