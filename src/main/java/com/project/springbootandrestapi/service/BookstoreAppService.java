26ce614ea177f8db0463f704c084dfbbef3e421a
HumeyraUnlu <75032647+HumeyraUnlu@users.noreply.github.com>
2021-07-26 15:47:16 +0300

    Update BookstoreAppService.java

--- src/main/java/com/project/springbootandrestapi/service/BookstoreAppService.java
+++ src/main/java/com/project/springbootandrestapi/service/BookstoreAppService.java
@@ -5,12 +5,15 @@ import java.util.List;
 import com.project.springbootandrestapi.entities.*;
 
 public interface BookstoreAppService {
-	List<BookstoreApp> getAll();
+	
+	public List<BookstoreApp> getAll();
+	public BookstoreApp getById(int id);
 	void add(BookstoreApp book);
 	void update(BookstoreApp book);
 	void delete(BookstoreApp book);
-	List<String> getCategories();	
+	public List<String> getCategories();	
 	public List<BookstoreApp> getCategory(String category);
-	List<String> getBookStores();
+	public List<String> getBookStores();
 	public List<BookstoreApp> getBookStore(String bookStore);
+	public List<BookstoreApp> getBookPrices(String name);
 }
